dfbc93f24bf9b2203d34bdd20a9a0ce7e5d7a903
A Fakhrurozi S <haifahrul@gmail.com>
2018-09-21 13:12:54 +0700

    fix bug

--- NumberValidation.js
+++ NumberValidation.js
@@ -1,23 +1,28 @@
-export default NumberValidation(val, column) {
-    let newText = "";
-    let numbers = "0123456789";
-    let isNumber = false;
-  
-    for (var i = 0; i < text.length; i++) {
-      if (numbers.indexOf(text[i]) > -1) {
-        newText = newText + text[i];
-        isNumber = true;
-      } else {
-        isNumber = false;
-      }
+/*
+ * haifahrul <haifahrul@gmail.com>
+ */
+
+const NumberValidation = (val, column) => {
+  let newText = "";
+  let numbers = "0123456789";
+  let isNumber = false;
+
+  for (var i = 0; i < text.length; i++) {
+    if (numbers.indexOf(text[i]) > -1) {
+      newText = newText + text[i];
+      isNumber = true;
+    } else {
+      isNumber = false;
     }
-  
-    if (isNumber === false) {
-      alert("please enter numbers only");
-      this.setState({ column: "" });
-      return;
-    }
-  
-    this.setState({ column: text });
   }
-  
+
+  if (isNumber === false) {
+    alert("please enter numbers only");
+    this.setState({ column: "" });
+    return;
+  }
+
+  this.setState({ column: text });
+};
+
+export default NumberValidation;
